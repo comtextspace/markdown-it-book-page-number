c19c2be62f461d35dd81ef00f11485d0b4ce0e88
KonstantinSh <Kverde@yandex.ru>
2022-12-07 23:44:00 +0500

    fix bug

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const link_class = "book-page-number";
 
 function processInline(token, state) {
   function str_to_token(str) {
-    let m = str.match(/\[\#\s*(\d+)\]/);
+    let m = str.match(/\[\#\s*(\d+)[^\]]*\]/);
     if (m === null) {
       let newToken = new state.Token("text", "", 0);
       newToken.content = str;
@@ -20,7 +20,9 @@ function processInline(token, state) {
     let child = token.children[i];
 
     if (child.type === "text" && child.content.indexOf("[#") !== -1) {
-      let items = child.content.split(/(\[\#\s*\d+\])/);
+      console.log(child.content);
+      let items = child.content.split(/(\[\#\s*\d+[^\]]*\])/);
+      console.log(items);
       let tokens = items.map(str_to_token);
       token.children.splice(i, 1, ...tokens);
       i--;
